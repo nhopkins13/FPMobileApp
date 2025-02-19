5b609d4d3b08edc37b432dfdc965a08dd484a377
Isabelle Neckel <ineckel@mines.edu>
2025-02-19 13:29:46 -0700

    Added empty files for screens

--- app/src/main/java/com/csci448/fpmobileapp/MainActivity.kt
+++ app/src/main/java/com/csci448/fpmobileapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.csci448.fpmobileapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
---
+++ app/src/main/java/com/csci448/fpmobileapp/ui/screens/WardrobeScreen.kt
@@ -0,0 +1,2 @@
+package com.csci448.fpmobileapp.ui.screens
+
