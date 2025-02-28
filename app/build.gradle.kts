d4823ffceccb5825db45809bdda3df94d5d6b6d5
Isabelle Neckel <ineckel@mines.edu>
2025-02-28 12:19:47 -0700

    added navigation dependencies, started navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation)
+    implementation(libs.kotlin.reflect)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/csci448/fpmobileapp/ui/navigation/specs/IScreenSpec.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/navigation/specs/IScreenSpec.kt
@@ -1,5 +1,9 @@
 package com.csci448.fpmobileapp.ui.navigation.specs
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
 /**
  * Interface to make ScreenSpecs for our app's Screens
  *
@@ -9,4 +13,16 @@ package com.csci448.fpmobileapp.ui.navigation.specs
  *  add content(viewmodel, navcontroller, modifier)
  */
 sealed interface IScreenSpec {
+    val route: String
+
+    @Composable
+    fun Content(navController: NavController, modifier: Modifier){
+
+    }
+
+    companion object{
+        val allScreens = IScreenSpec::class.sealedSubclasses.map{ it.objectInstance }
+        val root = "TODO? app name?"
+        val startDestination = "startup"
+    }
 }
