af9e23754d5c65d0294c25882b85254d96320dba
Neely Hopkins <nmhops@icloud.com>
2025-02-28 13:32:59 -0700

    added signup button

--- app/src/main/java/com/csci448/fpmobileapp/ui/screens/SignupScreen.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/screens/SignupScreen.kt
@@ -1,15 +1,20 @@
 package com.csci448.fpmobileapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.csci448.fpmobileapp.R
 import com.csci448.fpmobileapp.ui.components.Credential
 
 /** SignupScreen
@@ -24,7 +29,7 @@ import com.csci448.fpmobileapp.ui.components.Credential
  *
  **/
 @Composable
-fun SignupScreen(){
+fun SignupScreen(transportHome: () -> Unit){
     Column{
         //Box(modifier = Modifier.weight(0.7f).fillMaxSize()){}
         Box(modifier = Modifier.fillMaxWidth(),
@@ -47,12 +52,16 @@ fun SignupScreen(){
             contentAlignment = Alignment.Center) {
             Credential(1, "Confirm password")
         }
-        //Box(modifier = Modifier.weight(0.7f).fillMaxSize()){}
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Button(onClick = { transportHome() }) {
+                Text(text = stringResource(id = R.string.label_signup))
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 private fun PreviewSignupScreen(){
-    SignupScreen()
+    SignupScreen(transportHome = {})
 }
