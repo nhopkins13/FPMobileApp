3d95c1eb34271d3e022b1fefc33271d2dbd21598
Isabelle Neckel <ineckel@mines.edu>
2025-02-28 13:54:28 -0700

    filled in more of the NavBar

--- app/src/main/java/com/csci448/fpmobileapp/ui/components/NavBar.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/components/NavBar.kt
@@ -2,22 +2,31 @@ package com.csci448.fpmobileapp.ui.components
 
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.core.graphics.createBitmap
 
 @Composable
 fun NavBar(){
+    // TODO: pass in view model with status on which icon is selected
     BottomAppBar {
         BottomNavigation {
             BottomNavigationItem(
                 label = {
                     Text(
-                        text = ""
+                        text = "Home"
                     )
                 },
-                selected = false,
-                icon = {},
+                selected = false, // TODO: set status on which screen is active 
+                icon = {
+                    /*
+                    Icon(
+                        // TODO: fill in icon later
+                    )
+                     */
+                },
                 onClick = {}
             )
         }
