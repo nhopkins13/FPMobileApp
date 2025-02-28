50d2da469f27328ec7ee686e31699f3d114efdf2
Neely Hopkins <nmhops@icloud.com>
2025-02-28 13:37:46 -0700

    row/col outline for social

--- app/src/main/java/com/csci448/fpmobileapp/ui/screens/SocialScreen.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/screens/SocialScreen.kt
@@ -1,8 +1,19 @@
 package com.csci448.fpmobileapp.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 
 @Composable
-fun SocialScreen(){}
+fun SocialScreen(){
+    Column{
+        Row{
+            //search box to add new friends by user
+        }
+        Row{
+            //current friends in a list here
+        }
+    }
+}
 
 
