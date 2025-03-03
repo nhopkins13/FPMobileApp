1ca434def5d918c6625a6d74f93fe79220c1d5e4
Isabelle Neckel <ineckel@mines.edu>
2025-03-03 13:48:56 -0700

    put icons on the navbar buttons

--- app/src/main/java/com/csci448/fpmobileapp/ui/components/NavBar.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/components/NavBar.kt
@@ -2,11 +2,13 @@ package com.csci448.fpmobileapp.ui.components
 
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.core.graphics.createBitmap
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun NavBar(){
@@ -15,23 +17,86 @@ fun NavBar(){
         BottomNavigation {
             BottomNavigationItem(
                 label = {
-                    Text(
-                        text = "Home"
-                    )
+                    Text(text = "Home")
                 },
                 selected = false, // TODO: set status on which screen is active
                 icon = {
-                    /*
                     Icon(
-                        // TODO: fill in icon
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = "Home"
                     )
-                     */
                 },
                 onClick = {
                     // TODO: navigate to screen represented by button
                 }
             )
+            BottomNavigationItem(
+                label = {
+                    Text(text = "Shop")
+                },
+                selected = false,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.ShoppingCart,
+                        contentDescription = "Shop"
+                    )
+                },
+                onClick = {
+                    // TODO: navigate to shop
+                }
+            )
+            BottomNavigationItem(
+                label = {
+                    Text(text = "Clothing")
+                },
+                selected = false,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = "Clothing"
+                    )
+                },
+                onClick = {
+
+                }
+            )
+            BottomNavigationItem(
+                label = {
+                    Text(text = "Tasks")
+                },
+                selected = false,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.DateRange,
+                        contentDescription = "Tasks"
+                    )
+                },
+                onClick = {
+
+                }
+            )
+            BottomNavigationItem(
+                label = {
+                    Text(text = "Settings")
+                },
+                selected = false,
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Settings,
+                        contentDescription = "Settings"
+                    )
+                },
+                onClick = {
+
+                }
+            )
             // TODO: more BottomNavigationItems for each screen in menu
         }
     }
+}
+
+@Preview
+@Composable
+fun NavBarPreview(){
+    NavBar()
 }
