ab82ba2ca845d37ef75deb871e33ebc33df41a96
Neely Hopkins <nmhops@icloud.com>
2025-03-10 18:52:09 -0600

    updated start screen, changed nav buttons, and addded temporary dino logo

--- app/src/main/java/com/csci448/fpmobileapp/ui/components/NavButton.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/components/NavButton.kt
@@ -1,16 +1,44 @@
 package com.csci448.fpmobileapp.ui.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun NavButton(inPlaceText: String = "placeholder", buttonAction: ()->Unit){
-    Button(onClick = buttonAction) {
-        Text(inPlaceText)
+fun NavButton(inPlaceText: String = "placeholder", buttonAction: () -> Unit) {
+    Button(
+        onClick = buttonAction,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF4B0082)
+        ),
+        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 6.dp),
+        shape = RoundedCornerShape(4.dp),
+        modifier = Modifier.width(200.dp).height(50.dp)
+
+    ) {
+        Text(
+            text = inPlaceText,
+            color = Color.White
+        )
     }
 }
+
+
 
 @Preview
 @Composable
--- app/src/main/java/com/csci448/fpmobileapp/ui/screens/StartupScreen.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/screens/StartupScreen.kt
@@ -1,6 +1,11 @@
 package com.csci448.fpmobileapp.ui.screens
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,9 +14,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.csci448.fpmobileapp.R
 import com.csci448.fpmobileapp.data.Saurus
 import com.csci448.fpmobileapp.data.SaurusRepo
@@ -30,15 +40,30 @@ import com.csci448.fpmobileapp.ui.viewmodel.StudySaurusVM
 @Composable
 fun StartupScreen(viewModel : StudySaurusVM, goToHome: () -> Unit, goToLogin: () -> Unit, goToSignup: ()-> Unit, modifier: Modifier = Modifier){
     Column(modifier = modifier){
-        //Box(modifier = Modifier.weight(0.7f).fillMaxSize()){}
-        Box(modifier = Modifier.fillMaxWidth(),
+        Box(modifier = Modifier.fillMaxWidth().padding(16.dp),
             contentAlignment = Alignment.Center){
-            Text(stringResource(R.string.label_app_name))
+            Text(
+                text = stringResource(R.string.label_app_name),
+                fontWeight = FontWeight.Bold,
+                fontSize = 36.sp,
+                color = Color(0xFFFFA500)
+            )
         }
-        Box(modifier = Modifier.padding(vertical = 5.dp).fillMaxWidth(),
-            contentAlignment = Alignment.Center) {
-            Text("[logo/mascot goes here]")
+        Box(
+            modifier = Modifier
+                .padding(vertical = 16.dp, horizontal = 20.dp)
+                .widthIn(min = 500.dp, max = 500.dp)
+                .fillMaxHeight(0.75f)
+                .background(Color(0xFFADD8E6))
+                .border(width = 3.dp, color = Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            DinosaurImage()
         }
+
+
+        Spacer(modifier = Modifier.weight(1f))
+
         Box(modifier = Modifier.padding(vertical = 5.dp).fillMaxWidth(),
             contentAlignment = Alignment.Center) {
             NavButton(stringResource(id = R.string.label_login), { goToLogin() })
@@ -51,8 +76,17 @@ fun StartupScreen(viewModel : StudySaurusVM, goToHome: () -> Unit, goToLogin: ()
             contentAlignment = Alignment.Center) {
             NavButton(stringResource(id = R.string.label_offline), { goToHome() })
         }
-        //Box(modifier = Modifier.weight(0.7f).fillMaxSize()){}
     }
+}
+
+@Composable
+fun DinosaurImage() {
+        Image(
+            painter = painterResource(id = R.drawable.dinodrawing),
+            contentDescription = "Dinosaur Drawing",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(600.dp)
+        )
 }
 
 @SuppressLint("ViewModelConstructorInComposable")
