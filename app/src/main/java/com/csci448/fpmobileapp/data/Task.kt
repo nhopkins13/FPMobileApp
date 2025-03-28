cc655cbeb8178c0ad008e76d2ef736c14c6fa6da
Isabelle Neckel <ineckel@mines.edu>
2025-03-28 13:39:56 -0600

    Task card can display title, time, coins

---
+++ app/src/main/java/com/csci448/fpmobileapp/data/Task.kt
@@ -0,0 +1,9 @@
+package com.csci448.fpmobileapp.data
+
+import java.time.LocalDate
+
+data class Task (
+    val title: String,
+    val coins: Int,
+    val timeDue: String
+)
--- app/src/main/java/com/csci448/fpmobileapp/ui/components/TaskCard.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/components/TaskCard.kt
@@ -1,8 +1,37 @@
 package com.csci448.fpmobileapp.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.csci448.fpmobileapp.data.Task
 
 @Composable
-fun TaskCard(){
-    
+fun TaskCard(task: Task){
+    Card(modifier = Modifier) {
+        Row() {
+            Column() {
+                Text(
+                    text = task.title
+                )
+                Text(
+                    text = task.timeDue
+                )
+            }
+            Text(
+                text = task.coins.toString(),
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TaskCardPreview(){
+    TaskCard(Task(title="Homework", coins=10, timeDue="2:00pm"))
 }
