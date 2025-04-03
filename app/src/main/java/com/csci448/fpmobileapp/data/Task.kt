e1c09b06eda86a004c5eebb24c379b2edabcc6ea
Isabelle Neckel <ineckel@mines.edu>
2025-04-02 22:12:31 -0600

    Added a checkbox to task card

--- app/src/main/java/com/csci448/fpmobileapp/data/Task.kt
+++ app/src/main/java/com/csci448/fpmobileapp/data/Task.kt
@@ -1,9 +1,13 @@
 package com.csci448.fpmobileapp.data
 
+import android.util.MutableBoolean
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import java.time.LocalDate
 
 data class Task (
     val title: String,
     val coins: Int,
-    val timeDue: String
+    val timeDue: String,
+    val completed: MutableState<Boolean>
 )
--- app/src/main/java/com/csci448/fpmobileapp/ui/components/TaskCard.kt
+++ app/src/main/java/com/csci448/fpmobileapp/ui/components/TaskCard.kt
@@ -1,10 +1,13 @@
 package com.csci448.fpmobileapp.ui.components
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,6 +17,12 @@ import com.csci448.fpmobileapp.data.Task
 fun TaskCard(task: Task){
     Card(modifier = Modifier) {
         Row() {
+            Checkbox(
+                checked = task.completed.value,
+                onCheckedChange = {
+                    task.completed.value = !task.completed.value
+                }
+            )
             Column() {
                 Text(
                     text = task.title
@@ -23,15 +32,16 @@ fun TaskCard(task: Task){
                 )
             }
             Text(
-                text = task.coins.toString(),
+                text = "$" + task.coins.toString(),
                 modifier = Modifier.align(Alignment.CenterVertically)
             )
         }
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Preview
 @Composable
 fun TaskCardPreview(){
-    TaskCard(Task(title="Homework", coins=10, timeDue="2:00pm"))
+    TaskCard(Task(title="Homework", coins=10, timeDue="2:00pm", completed = mutableStateOf(false)))
 }
